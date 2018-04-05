2b1a84553ccfae9e8d8f92b1e195d4c81627f1af
leewens1303 <37306410+leewens1303@users.noreply.github.com>
2018-04-05 19:44:07 +0530

    Add files via upload

---
+++ Answer1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct node 
+{
+	int value;
+	struct node*left, *right;
+}
+
+void output(struct node*){
+    
+    inorder traversal geeksforgeeks
+}
+
+	
+	
+int main() {
+    int n;
+    
+	
+	return 0;
+}
