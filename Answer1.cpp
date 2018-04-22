eb6c8315b132f4f5298784839982965b4924a363
leewens1303 <37306410+leewens1303@users.noreply.github.com>
2018-04-23 01:35:51 +0530

    Update Answer1.cpp

--- Answer1.cpp
+++ Answer1.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
 	int data;
-	struct node* left, right;
+	struct node* left;
+	struct node* right;
 };
 
 struct node* newNode(int data)
 {
 	struct node* node = (struct node*)
-				    malloc(sizeof(struct node));
+								malloc(sizeof(struct node));
 	node->data = data;
 	node->left = NULL;
 	node->right = NULL;
@@ -18,18 +19,29 @@ struct node* newNode(int data)
 	return(node);
 }
 
+void printInorder(struct node* node)
+{
+	if (node == NULL)
+		return;
+
+	printInorder(node->left);
+
+	printf("%d ", node->data); 
+
+	printInorder(node->right);
+}
 
 int main()
 {
 	struct node *root = newNode(1);
-	root->left= newNode(2);
-	root->right= newNode(3);
-	root->left->left= newNode(4);
+	root->left			 = newNode(2);
+	root->right		 = newNode(3);
+	root->left->left	 = newNode(4);
 	root->left->right = newNode(5); 
 
-	printf("\nInorder traversal of tree is \n");
+	printf("\nInorder traversal of binary tree is \n");
 	printInorder(root); 
 
 	getchar();
-    	return 0;
+	return 0;
 }
